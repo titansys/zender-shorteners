f362287c0349bf4ab45b444a63e708d25d33c151
Norielle Cruz <mail@noriellecruz.com>
2022-05-06 06:14:27 +0800

    Updated Premium URL Shortener

--- premium-url-shortener.php
+++ premium-url-shortener.php
@@ -12,16 +12,18 @@ function shortenUrl($url, &$system){
 	 * @return false:Failed
 	 */
  
-	$accessToken = "ACCESS_TOKEN"; // API token/key
+	$apiKey = "API_KEY"; // API key
 	$urlSystem = "SITE_URL"; // Your premium url shortener site url. eg. https://mydomain.com
+	$customDomain = "CUSTOM_DOMAIN"; // Custom domain, remove value if no custom domain
  
 	$data = json_decode($system->guzzle->post("{$urlSystem}/api/url/add", [
 		"headers" => [
-			"Authorization" => "Bearer {$accessToken}",
+			"Authorization" => "Bearer {$apiKey}",
 		    "Content-Type: application/json"
 		],
 		"json" => [
-			"url" => $url
+			"url" => $url,
+			"domain" => empty($customDomain) ? false : $customDomain
 		],
         "allow_redirects" => true,
         "http_errors" => false
