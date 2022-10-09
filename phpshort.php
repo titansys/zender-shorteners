080451a69eb33b85cb0bef53b5e5ab4d1949e567
Norielle Cruz <mail@noriellecruz.com>
2022-10-09 16:11:32 +0800

    fix: phpshort controller protocol bug

--- phpshort.php
+++ phpshort.php
@@ -15,7 +15,6 @@ function shortenUrl($url, &$system){
 	$apiKey = "API_KEY"; // API Key from a phpshort account
 	$domainId = 1; // (int) The domain ID the link to be saved under.
 	$siteUrl = "https://mydomain.com"; // Your phpshort site url. eg. https://mydomain.com
-	$protocol = "https"; // Your phpshort site protocol, https or http
 
 	$shorten = json_decode($system->guzzle->post("{$siteUrl}/api/v1/links", [
 		"headers" => [
@@ -29,5 +28,5 @@ function shortenUrl($url, &$system){
         "http_errors" => false
 	])->getBody()->getContents(), true);
 
-	return $shorten["status"] == 200 ? "{$protocol}://{$shorten["data"]["short_url"]}" : false;
+	return $shorten["status"] == 200 ? "{$shorten["data"]["short_url"]}" : false;
 }
